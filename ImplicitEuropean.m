5c1c016f1a792a1110bd49399d762d520499a246
Dazhi  Liao <liaodazh@p189.ecf.utoronto.ca>
2015-10-14 18:00:33 -0400

    Refactored explicit method and redoing implicit.

--- ImplicitEuropean.m
+++ ImplicitEuropean.m
@@ -1,51 +1,58 @@
 function  [ P ] = ImplicitEuropean( S, tau, E, r, sigma )
-%UNTITLED3 Summary of this function goes here
+% ImplicitEuropean: Computes the fair value of the European put option
+% using the implicit Euler method.
 %   Detailed explanation goes here
 numPartitionsX = 500;
 numPartitionsT = 500;
 
-r_days = r / 365;
+% determine the maximum and minimum x values.
 z = 3; % variable for random walk
-S_t = S * exp((r_days - 0.5 * sigma^2) * tau + sigma * sqrt(tau) * z);
+S_t = S * exp((r - 0.5 * sigma^2) * tau + sigma * sqrt(tau) * z);
 x_max = log(S_t / S);
 x_min = -x_max;
-dx = (x_max - x_min) / numPartitionsX;
-dt = tau / numPartitionsT;
 
+% compute the partition size dt and dx.
+dt = tau / numPartitionsT;
+dx = (x_max - x_min) / numPartitionsX;
+
+% compute coefficients A, B, and C.
+A_scalar = (r * dt / (2 * dx) - sigma ^ 2 * dt / (2 * dx ^ 2));
+A = ones(numPartitionsX - 1, 1);
+A = A * A_scalar;
+B_scalar = (sigma ^ 2 * dt / dx ^ 2 + r * dt + 1);
+B = ones(numPartitionsX - 1, 1);
+B = B * B_scalar;
+C_scalar = (-(r * dt) / (2 * dx) - (sigma ^ 2 * dt) / (2 * dx ^ 2));
+C = ones(numPartitionsX - 1, 1);
+C = C * C_scalar;
+
+% generate a tri-diagonal matrix T, of size numPartitionsX - 1.
+T = spdiags([A, B, C], [-1, 0, 1], numPartitionsX - 1, numPartitionsX - 1);
+
+% allocate memory for the solution mesh
 PRICE = zeros(numPartitionsT + 1, numPartitionsX + 1);
 X = linspace(x_min, x_max, numPartitionsX + 1);
 T = linspace(0, tau, numPartitionsT + 1);
 
-% initial condition
-PRICE(1,:) = max(E - S * exp(X), 0);
-% boundary condition at x = -inf
-PRICE(:,1) = E * exp(-r_days * T);
-% boundary condition at x = inf
-PRICE(:,numPartitionsX + 1) = 0;
+% initial condition...
+PRICE(1, :) = max(E - S * exp(X), 0);
 
-% coefficients
-A = 0.5 * dt * sigma ^ 2 / dx^2 - 0.5 * r_days * dt / dx;
-B = 1 - sigma ^ 2 * dt / dx^2 - r_days * dt/ 2 / dx;
-C = 0.5 * dt * sigma^2 / dx^2 + 0.5 * r_days * dt / dx;
+% boundary value at X = -inf
+PRICE(:, 1) = E * exp(-r * T); % present value of payoff
+% boundary value at X = inf
+PRICE(:, end) = 0;
 
-% generate tri-diagonal matrix
-Acoeffs = zeros(numPartitionsX + 1, 1) + A;
-Bcoeffs = zeros(numPartitionsX + 1, 1) + B;
-Ccoeffs = zeros(numPartitionsX + 1, 1) + C;
-tri = diag(Acoeffs(2:end), -1) + diag(Bcoeffs) + diag(Ccoeffs(1:end-1), +1);
-tri_inv = inv(tri);
-
-% implicit iteration
-for t = 1:numPartitionsT
-    temp = zeros(numPartitionsT + 1, 1);
-    temp(1) = A * PRICE(t+1, 1);
-    temp(end) = C * PRICE(t+1, numPartitionsT);
-    b = PRICE(t,:)' -temp;
-    temp = tri_inv * b;
-    PRICE(t+1, (2:end-1)) = temp(2:end-1);
+P_boundary = zeros(numPartitionsX - 1, 1);
+% populate the solution mesh
+for i = 2:numPartitionsT + 1
+    P_boundary(1) = A_scalar * PRICE(i, 1);
+    P_boundary(end) = C_scalar * PRICE(i, end);
+    % we want to solve the equation: T * P(t = t) = P(t = t - 1) + P_boundary(t = t)
+    PRICE(i, 2:numPartitionsX) = ...
+        (PRICE(i-1, 2:numPartitionsX) - P_boundary) \ T;
 end
 
-P = PRICE(numPartitionsT+1, ceil((numPartitionsX + 1)/2));
+P = PRICE(numPartitionsT+1, ceil((numPartitionsX + 1) / 2));
 
 end
 
