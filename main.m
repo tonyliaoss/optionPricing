80e04d08c165a6020e065728432dc97bcb10ad0e
Dazhi  Liao <liaodazh@p187.ecf.utoronto.ca>
2015-10-30 11:44:08 -0400

    Some minor changes to printouts.

--- main.m
+++ main.m
@@ -16,28 +16,31 @@ fprintf(formatString, S, E, r, sigma, tau);
 % start off by evaluating European options
 fprintf('Evaluating pricing of European options...\n');
 
-tic
-fprintf('Using simple matrix inversion for solving linear systems...\n');
+fprintf('  Calculating the closed-form solution and explicit Euler solutions...\n');
 % evaluating "simple" numerical methods
 bse = BSEqnEuropean(S, tau, E, r, sigma);
 addpath('simple');
 ee = ExplicitEuropean(S, tau, E, r, sigma);
+fprintf('  Using builtin matrix inversion for solving linear systems...\n');
+tic
 ie = ImplicitEuropean(S, tau, E, r, sigma);
 cne = CrankNicolsonEuropean(S, tau, E, r, sigma);
+fprintf('    ');
 toc
 
-tic
-fprintf('Using SOR for solving linear systems...\n');
+fprintf('  Using SOR for solving linear systems...\n');
 % evaluating European options with successive over-relaxation...
 addpath('sor');
 sor_ee = NaN; % doesn't apply here. SOR only solves linear systems.
+tic
 sor_ie = SORImplicitEuropean(S, tau, E, r, sigma);
 sor_cne = SORCrankNicolsonEuropean(S, tau, E, r, sigma);
+fprintf('    ');
 toc
 
 % Generate a table.
 methodNames = {'Black-Scholes'; 'Explicit'; 'Implicit'; 'Crank-Nicolson'};
 simple = [bse; ee; ie; cne];
 sor = [bse; sor_ee; sor_ie; sor_cne];
-tbl = table(simple, sor, 'RowNames', methodNames)
+euroTable = table(simple, sor, 'RowNames', methodNames)
 
